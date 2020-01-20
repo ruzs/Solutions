9883f5e0162608895ca4b5d42819aa713f7e85a5
ruzs <ruz@outlook.kr>
2020-01-20 22:47:09 +0900

    서울 지하철 2호선

--- source.cc
+++ source.cc
@@ -2,33 +2,50 @@
 
 using namespace std;
 
-int n;
-set<int> g[100001];
-vector<int> d, order;
-void dfs(int a, vector<int>::iterator& it) {
-	d[a] = 1;
-	while (it != order.end() && g[a].count(*it)) {
-		int b = *it++;
-		if (!d[b]) dfs(b, it);
+queue<int> q;
+vector<int> g[3001];
+int d[3001];
+bool v[3001];
+//return starting cycle node
+int dfs(int a, int p) {
+	v[a] = 1;
+	for(int b : g[a]) {
+		if (b == p) continue;
+		
+		if (v[b]) {
+			q.push(a);
+			d[a] = 0;
+			return b;
+		}
+		int r = dfs(b, a);
+		if (r > 0) {
+			q.push(a);
+			d[a] = 0;
+			return r == a ? -1 : r;
+		}
 	}
+	return -1;
 }
 int main() {
+	int n;
 	cin >> n;
-	d.resize(n + 1);
-	for(int i =1; i<n; ++i) {
+	for(int i =0; i<n; ++i) {
 		int a, b;
 		cin >> a >> b;
-		g[a].insert(b);
-		g[b].insert(a);
+		g[a].push_back(b);
+		g[b].push_back(a);
 	}
-	order.resize(n);
-	for(int &i : order) cin >> i;
-	auto it = order.begin();
-	
-	if (*it != 1) return cout << 0, 0;
-	
-	++it;
-	dfs(1, it);
+	memset(d, -1, sizeof d);
+	dfs(1, 0);
 
-	cout << (it == order.end());
+	while(q.size()) {
+		int a = q.front(); q.pop();
+		for(int b : g[a]) {
+			if (d[b] == -1) {
+				d[b] = d[a] + 1;
+				q.push(b);
+			}
+		}
+	}
+	for(int i =1; i<=n; ++i) cout << d[i] << ' ';
 }
