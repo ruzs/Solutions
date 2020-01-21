dcdbc4fd45c65ed2d19515c5e617ef5514374b81
ruzs <ruz@outlook.kr>
2020-01-21 17:01:24 +0900

    가르침

--- source.cc
+++ source.cc
@@ -2,51 +2,29 @@
 
 using namespace std;
 
-int n, m, a[501][501];
-
 int main() {
+	const int enc[] = {21,0,22,1,2,3,4,5,23,6,7,8,9,24,10,11,12,13,14,25,15,16,17,18,19,20};
+	int n, m, a[51]{};
+	char s[16];
 	cin >> n >> m;
-	int sq = (int)(sqrt((double)(n*m))+0.5);
-	if (sq*sq != n*m || m > n) return cout << -1, 0;
-	m = 0;
-	for(int i =1; i<=n; ++i) {
-		for(int j =sq*(i-1)+1; j<=sq*i; ++j) {
-			a[i][j%n ? j%n : n] = 1;
-			m++;
+	m -= 5;
+	if (m < 0) return cout << 0, 0;
+	for(int i =0; i<n; ++i) {
+		cin >> s;
+		for(int j =0; s[j]; ++j) {
+			a[i] |= (1 << enc[s[j] - 'a']);
+		}
+		a[i] &= (1 << 21) - 1;
+	}
+	int ans = 0;
+	for(int s =0; s<1<<21; ++s) {
+		if (__builtin_popcount(s) == m) {
+			int cnt = 0;
+			for(int i =0; i<n; ++i) {
+				if((s & a[i]) == a[i]) cnt++;
+			}
+			ans = max(ans, cnt);
 		}
 	}
-	cout << m << '\n';
-	for(int i =1; i<=n; ++i) {
-		for(int j =1; j<=n; ++j) {
-			if (a[i][j]) cout << i << ' ' << j << '\n';
-		}
-	}
+	cout << ans;
 }
-// #include <bits/stdc++.h>
-
-// using namespace std;
-
-// using vi = vector<int>;
-// using matrix = vector<vi>;
-// matrix mul(matrix& a) {
-// 	int n =a.size();
-// 	matrix r(n, vi(n, 0));
-// 	for(int i =0; i<n; ++i)
-// 		for(int j =0; j<n; ++j)
-// 			for(int k =0; k<n; ++k)
-// 				r[i][j] += a[i][k] * a[k][j];
-// 	return r;
-// }
-// int main() {
-// 	int n, m; cin >> n >> m;
-// 	matrix a(n, vi(n));
-// 	for(int i =0; i<m; ++i) {
-// 		int r, c; cin >> r >> c;
-// 		a[r - 1][c - 1] = 1;
-// 	}
-// 	auto r = mul(a);
-// 	for(auto & i : r) {
-// 		for(int j : i) cout << j << ' ';
-// 		cout << endl;
-// 	}
-// }
