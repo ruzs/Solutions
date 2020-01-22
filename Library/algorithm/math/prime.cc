c251b53bada744c0bbe41b0e745036ea3b0e0ad1
ruzs <ruz@outlook.kr>
2020-01-22 17:39:57 +0900

    s

--- Library/algorithm/math/prime.cc
+++ Library/algorithm/math/prime.cc
@@ -2,14 +2,13 @@ const int N = 1e7+5;
 
 bool np[N>>1];
 
-auto primeList(int max_n) {
-	vector<int> r={2};
-	long long i=1, j, k;
+inline auto primeList(int max_n) {
+	vector<long long> r = {2};
+	long long i =1, j, k;
 	while(i+=2 <= max_n) {
 		if (np[i>>1]) continue;
 		r.push_back(i);
-		for(j=i*i, k=i+i; j<=max_n; j+=k)
-			np[j>>1] = 1;
+		for(j=i*i, k=i+i; j<=max_n; j+=k) np[j>>1] = 1;
 	}
 	return r;
 }
