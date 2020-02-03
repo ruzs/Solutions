c1afb8aba670212c1c48296ab6e1a18d5553d87c
ruzs <ruz@outlook.kr>
2020-02-04 05:44:25 +0900

    .

--- source.cc
+++ source.cc
@@ -2,22 +2,27 @@
 
 using namespace std;
 
+void solve() {
+	int j, n, a[1000];
+	cin >> j >> n;
+	for(int i =0; i<n; ++i) {
+		int b, c;
+		cin >> b >> c;
+		a[i] = b * c;
+	}
+	sort(a, a + n);
+	int cnt =0;
+	for(int i =n-1; ~i; --i) {
+		j -= a[i];
+		cnt++;
+		if (j <= 0) break;
+	}
+	cout << cnt << '\n';
+}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n;
-	cin >> n;
-	tuple<int, int, int> p[900];
-	for(int i =0; i<3*n; ++i) {
-		int y, x;
-		cin >> y >> x;
-		p[i] = {y, x, i + 1};
-	}
-	sort(p, p + 3*n);
-	for(int i =0; i<n*3; i+=3) {
-		cout << get<2>(p[i]) << ' '
-			<< get<2>(p[i + 1]) << ' '
-			<< get<2>(p[i + 2]) << '\n';
-	}
+	int T, t =0; cin >> T;
+	while(++t <= T) solve();
 }
