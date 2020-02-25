7d7bb035e67879eb4c37847a2a7ab5789ac597da
ruzs <ruz@outlook.kr>
2020-02-25 23:52:22 +0900

    .

--- source.cc
+++ source.cc
@@ -5,22 +5,7 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	vector<int> a(n), b(m);
-	for(int i =0; i<n; ++i) cin >> a[i];
-	for(int i =0; i<m; ++i) cin >> b[i];
-	sort(a.begin(), a.end());
-	sort(b.begin(), b.end());
-	if (a.size() < b.size()) swap(a, b), swap(n, m);
-	vector<vector<int>> d(n, vector<int>(m, -1));
-	auto dp = [&](auto& dp, int x, int y) {
-		if (y == m) return 0;
-		if (x == n) return (int)1e9;
-		int & ref = d[x][y];
-		if (~ref) return ref;
-		ref = min(dp(dp, x + 1, y), dp(dp, x + 1, y + 1) + abs(a[x] - b[y]));
-		return ref;
-	};
-	cout << dp(dp, 0, 0);
+	int n;
+	cin >> n;
+	cout << n;
 }
