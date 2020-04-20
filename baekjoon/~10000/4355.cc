5a0623dd9e07d005facf5bb4175ff234e5087304
ruzs <ruz@outlook.kr>
2020-04-20 17:31:18 +0900

    서로소

--- baekjoon/~10000/4355.cc
+++ baekjoon/~10000/4355.cc
@@ -2,23 +2,21 @@
 
 using namespace std;
 
-//Euler phi function, phi(x) = x보다 작은 서로소의 개수
-int getEulerPhi(int x) {
-	int phi = x;
-	for(int i =2; i*i<=x; ++i) {
-		if (x % i == 0) phi /= i, phi *= i - 1;
-		while(x % i == 0) x /= i;
-	}
-	if (x > 1) phi /= x, phi *= x - 1;
-	return phi;
-}
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
 
 	int n;
 	while(cin >> n) {
 		if (!n) break;
-		cout << getEulerPhi(n) << '\n';
+		int r = n;
+		for(int i =2; i*i<=n; ++i) {
+			if (n % i == 0) {
+				while(n % i == 0) n /= i;
+				r -= r / i;
+			}
+		}
+		if (n > 1) r -= r / n;
+		cout << r << '\n';
 	}
 }
