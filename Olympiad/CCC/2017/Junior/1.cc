cd1d37af8c0574f6d298f01bff310c695aa9197c
ruzs <ruz@outlook.kr>
2020-05-02 18:37:11 +0900

    사분면 고르기

---
+++ Olympiad/CCC/2017/Junior/1.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int x, y;
+    cin >> x >> y;
+    if (x < 0 && y < 0) cout << 3;
+    else if (x < 0) cout << 2;
+    else if (y < 0) cout << 4;
+    else cout << 1;
+}
