b309d77e1a557079e2a232a6febad8b9698604ec
ruzs <ruz@outlook.kr>
2020-05-03 11:42:27 +0900

    이전 순열

---
+++ baekjoon/~20000/10973.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int & i : a) cin >> i;
+    if (next_permutation(a.begin(), a.end())) {
+        for(int i : a) cout << i << ' ';
+    } else {
+        cout << -1;
+    }
+}
