f93fd470d27d653d16d8f152063bfb9224c15193
ruzs <ruz@outlook.kr>
2020-05-23 14:46:52 +0900

    팰린드롬 개수 구하기 (Small)

---
+++ baekjoon/~20000/14505.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+    int n = s.size();
+    int d[31][31];
+    memset(d, -1, sizeof d);
+    function<int(int, int)> dp = [&](int l, int r) {
+        int & ref = d[l][r];
+        if (~ref) return ref;
+        if (l > r) return ref = 0;
+        if (l == r) return ref = 1;
+        ref = dp(l + 1, r) + dp(l, r - 1) - dp(l + 1, r - 1);
+        if (s[l] == s[r]) ref += dp(l + 1, r - 1) + 1;
+        return ref = (ref % 10007 + 10007) % 10007;
+    };
+    cout << dp(0, n - 1);
+}
