dd17b9e4911612291f5aed3a843897d67530456b
ruzs <ruz@outlook.kr>
2020-05-26 01:55:20 +0900

    선분 교차 2

--- baekjoon/~20000/17387.cc
+++ baekjoon/~20000/17387.cc
@@ -2,24 +2,30 @@
 
 using namespace std;
 
-using point = complex<long long>;
-int ccw(point a, point b, point c) {
-	long long r = (conj(b - a) * (c - b)).imag();
-	return r ? r < 0 ? -1 : 1 : 0;
-}
-bool cross(point a, point b, point c, point d) {
-	return (ccw(a, c, b) * ccw(a, d, b)) <= 0;
-}
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-	int a, b, c, d, e, f, g, h;
-	cin >> a >> b >> c >> d >> e >> f >> g >> h;
-	if (a > c) swap(a, c), swap(b, d);
-	if (e > g) swap(e, g), swap(f, h);
-	
-	if (c < e || g < a) cout << 0;
-	else cout << (cross({a, b}, {c, d}, {e, f}, {g, h}) &&
-			cross({e, f}, {g, h}, {a, b}, {c, d}));
+    struct pt {
+        long long x, y;
+        pt operator-(const pt & b) {
+            return {x - b.x, y - b.y};
+        }
+        bool operator<(const pt & b) const {
+            return x < b.x || x == b.x && y < b.y;
+        }
+    };
+    auto cross = [](pt a, pt b) {
+        return a.x * b.y - b.x * a.y;
+    };
+    auto ccw = [=](pt a, pt b, pt c) {
+        auto cw = cross(b - a, c - b);
+        return cw ? cw < 0 ? -1 : 1 : 0;
+    };
+
+    pt a, b, c, d;
+    cin >> a.x >> a.y >> b.x >> b.y >> c.x >> c.y >> d.x >> d.y;
+    if (ccw(a, b, c) * ccw(a, b, d) > 0 || ccw(c, d, a) * ccw(c, d, b) > 0) cout << 0;
+    else if (cross(a - b, c - d) == 0 && (max(a, b) < min(c, d) || max(c, d) < min(a, b))) cout << 0;
+    else cout << 1;
 }
