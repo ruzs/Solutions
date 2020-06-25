2c6e6b9213a3874bd4e27d666a3a610bc3b02b92
ruzs <ruz@outlook.kr>
2020-06-25 14:55:18 +0900

    숫자 맞추기

--- Olympiad/KOI/지역본선/2009/초등부/5.cc
+++ Olympiad/KOI/지역본선/2009/초등부/5.cc
@@ -11,23 +11,39 @@ int main() {
 	string a, b;
 	cin >> a >> b;
 	int d[10005][10];
+	pair<int, int> f[10005][10];
 	memset(d, 64, sizeof d);
 	d[0][0] = 0;
 	for(int i =0; i<n; ++i) {
 		for(int j =0; j<10; ++j) {
 			if (d[i][j] < 1e9) {
 				int x = (b[i] & 15) - ((a[i] & 15) + j) % 10;
-				if (x == 0) d[i + 1][j] = min(d[i + 1][j], d[i][j]);
-				else if (x < 0) {
-					d[i + 1][(j + x + 10) % 10] = min(d[i + 1][(j + x + 10) % 10], d[i][j] + (x + 10) % 10);
-					d[i + 1][j] = min(d[i + 1][j], d[i][j] - x);
+				auto cmp = [&](int ti, int tj, int fi, int fj, int c) {
+					if (d[ti][tj] > d[fi][fj] + abs(c)) {
+						d[ti][tj] = d[fi][fj] + abs(c);
+						f[ti][tj] = {fj, c};
+					}
+				};
+				if (x == 0) {
+					cmp(i + 1, j, i, j, 0);
+				} else if (x < 0) {
+					cmp(i + 1, (j + x + 10) % 10, i, j, (x + 10) % 10);
+					cmp(i + 1, j, i, j, x);
 				}
 				else {
-					d[i + 1][(j + x) % 10] = min(d[i + 1][(j + x) % 10], d[i][j] + x);
-					d[i + 1][j] = min(d[i + 1][j], d[i][j] + 10 - x);
+					cmp(i + 1, (j + x) % 10, i, j, x);
+					cmp(i + 1, j, i, j, -(10 - x));
 				}
 			}
 		}
 	}
-	cout << *min_element(&d[n][0], &d[n][10]);
+	auto j = min_element(&d[n][0], &d[n][10]) - &d[n][0];
+	cout << d[n][j] << '\n';
+	vector<int> ans;
+	for(int i =n; i>0; --i) {
+		ans.push_back(f[i][j].second);
+		j = f[i][j].first;
+	}
+	reverse(ans.begin(), ans.end());
+	for(int i =0; i<ans.size(); ++i) cout << i + 1 << ' ' << ans[i] << '\n';
 }
