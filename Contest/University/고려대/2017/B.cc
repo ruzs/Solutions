02a161408a56bd0a977de8651f1d2615a2aaced9
ruzs <ruz@outlook.kr>
2020-06-30 02:53:13 +0900

    팬들에게 둘러싸인 홍준

---
+++ Contest/University/고려대/2017/B.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string s;
+	cin >> s;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << s << "::fan:\n";
+	cout << ":fan::fan::fan:";
+}
