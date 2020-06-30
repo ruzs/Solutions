51365609341ea4495762ce1863335685da1d5b2a
ruzs <ruz@outlook.kr>
2020-06-30 14:17:59 +0900

    나는 행복합니다~

---
+++ baekjoon/~20000/14652.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m, x;
+	cin >> n >> m >> x;
+	cout << x / m << ' ' << x % m;
+}
