12d270650a34f0cab293ad30bad73a10c64171cb
ruzs <ruz@outlook.kr>
2020-08-28 11:33:23 +0900

    합 구하기

---
+++ baekjoon/~20000/11441.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> p = {0};
+	while (n--) {
+		int x; cin >> x;
+		p.push_back(p.back() + x);
+	}
+	int m;
+	cin >> m;
+	while (m--) {
+		int i, j;
+		cin >> i >> j;
+		cout << p[j] - p[i - 1] << '\n';
+	}
+}
