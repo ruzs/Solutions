fb62aa22b2914fb68e9e197c016c64c0e6c719a0
ruzs <ruz@outlook.kr>
2020-08-28 10:28:42 +0900

    PROZOR

--- baekjoon/~20000/11955.cc
+++ baekjoon/~20000/11955.cc
@@ -4,35 +4,41 @@ using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(NULL);
 
 	int n, m, k;
 	cin >> n >> m >> k;
-	vector<string> g(n);
-	for(int i =0; i<n; ++i) cin >> g[i];
-	int mx = 0, sy, sx;
-	for(int i =0; i+k<=n; ++i) {
-		for(int j =0; j+k<=m; ++j) {
-			int sum =0;
-			for(int y =1; y+1<k; ++y) {
-				for(int x =1; x+1<k; ++x) {
-					sum += g[i + y][j + x] == '*';
-				}
-			}
-			if (mx < sum) {
-				mx = sum;
-				sy = i;
-				sx = j;
+	vector<string> a(n);
+	for(string & i : a) cin >> i;
+	vector<vector<int>> d(n + 1, vector<int>(m + 1));
+	for(int i =0; i<n; ++i) {
+		for(int j =0; j<m; ++j) {
+			d[i + 1][j + 1] = d[i + 1][j] + d[i][j + 1] - d[i][j] + (a[i][j] == '*');
+		}
+	}
+	int py, px, mx = -1, cnt = 0;
+	for(int i =1; i+k-1<=n; ++i) {
+		for(int j =1; j+k-1<=m; ++j) {
+			cnt = d[i+k-2][j+k-2] - d[i][j+k-2] - d[i+k-2][j] + d[i][j];
+			if (mx < cnt) {
+				mx = cnt;
+				py = i-1;
+				px = j-1;
 			}
 		}
 	}
-	for(int i =1; i+1<k; ++i) {
-		g[sy + i][sx] = '|';
-		g[sy + i][sx + k - 1] = '|';
-		g[sy][sx + i] = '-';
-		g[sy + k - 1][sx + i] = '-';
+	a[py][px] = '+';
+	a[py][px+k-1] = '+';
+	a[py+k-1][px] = '+';
+	a[py+k-1][px+k-1] = '+';
+	for(int i =1; i<k-1; ++i) {
+		a[py][px + i] = '-';
+		a[py+k-1][px + i] = '-';
+		a[py + i][px] = '|';
+		a[py + i][px+k-1] = '|';
 	}
-	g[sy][sx] = g[sy + k - 1][sx] = g[sy][sx + k - 1] = g[sy + k - 1][sx + k - 1] = '+';
 	cout << mx << '\n';
-	for(auto & i : g) cout << i << '\n';
+	for(int i =0; i<n; ++i) {
+		cout << a[i] << '\n';
+	}
 }
